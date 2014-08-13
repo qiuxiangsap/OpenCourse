5e9a5dfd8111ca103ec9f2bfdff85d3d7f2b496e
unknown <I076746@PVGN50868922A.apj.global.corp.sap>
2014-08-13 13:48:30 +0800

    SimSTL: Add implementation of QuickSort and Merge Sort

--- SimSTL/sort.cpp
+++ SimSTL/sort.cpp
@@ -15,6 +15,7 @@
 
 using namespace std;
 
+
 namespace Algorithm {
 	template <class T>
 	void exch(T * arr, const T& i, const T& j) {
@@ -34,31 +35,86 @@ namespace Algorithm {
 		}
 	}
 
-	void bubble_sort(int *arr, int len) {
+	void merge(int *arr, int low1, int high1, int low2, int high2) {
+		int len = high1 - low1 + 1 + high2 - low2 + 1;
+		int *u_sorted = new int[len];
+
+		int i = low1;
+		int j = low2;
+		int num = 0;
+		while (i <= high1 && j <= high2 ) {
+			if (arr[i] < arr[j]) {
+				u_sorted[num++] = arr[i];
+				i++;
+			}  else {
+				u_sorted[num++] = arr[j];
+				j++;
+			}
+		}
+
+		while (i <=high1) {
+			u_sorted[num++] = arr[i++];
+		}
+		while (j <= high2 ) {
+			u_sorted[num++] = arr[j++];
+		}
+
+		for (int k = low1; k <= high1; k++) {
+			arr[k] = u_sorted[k - low1];
+		}
+		for (int k = low2; k <= high2; k++) {
+			arr[k] = u_sorted[high1 - low1 + k - low2 + 1];
+		}
+		delete [] u_sorted;
+	}
+	
+	void _merge_sort(int *arr, int low, int high) {
+		if (low >= high) return;
+		int mid = (low + high) / 2;
+		_merge_sort(arr, low, mid);
+		_merge_sort(arr, mid + 1, high);
+		merge(arr, low, mid, mid + 1, high);
+
 	}
 
-	void select_sort(int *arr, int len) {
+	void merge_sort(int *arr, int len) {
+		_merge_sort(arr, 0, len - 1);
 	}
 
-	void shell_sort(int *arr, int len) {
+	void _qsort(int *arr, int low, int high) {
+		if (low >= high) {
+			return;
+		}
+		int key = arr[low];
+		int first = low;
+		int last = high;
+
+		while (first < last) {
+			while (first < last && arr[last] >= key) last--;
+			arr[first] = arr[last];
+			while (first < last && arr[first] <= key) first++;
+			arr[last] = arr[first];
+		}
+		arr[first] = key;
+		_qsort(arr, low, first - 1);
+		_qsort(arr, first+1, high);
 	}
 
-	void qsort(int *arr, int len) {
+	void q_sort(int *arr, int len) {
+		_qsort(arr, 0, len - 1);
 	}
 }
 
 int main(int argc, char*argv[]) 
 {
-	//int arr[] = {10, 8, 4, 2};
-	//copy(arr, arr + sizeof(arr) / sizeof(arr[0]), ostream_iterator<int>(cout, " "));
-	//insert_sort<int>(arr, sizeof(arr) / sizeof(arr[0]));
-	//copy(arr, arr + sizeof(arr) / sizeof(arr[0]), ostream_iterator<int>(cout, " "));
-	int id;
-#pragma omp parallel private(id) 
-	{
-		id = omp_get_thread_num();
-		cout << id << endl;
-	}
+	int arr[] = {10, 8, 4, 2};
+	copy(arr, arr + sizeof(arr) / sizeof(arr[0]), ostream_iterator<int>(cout, " "));
+	//Algorithm::q_sort(arr, sizeof(arr) / sizeof(arr[0]));
+	Algorithm::merge_sort(arr, sizeof(arr) / sizeof(arr[0]));
+	cout << endl;
+	copy(arr, arr + sizeof(arr) / sizeof(arr[0]), ostream_iterator<int>(cout, " "));
+
+
 	return 0;
 }
 
